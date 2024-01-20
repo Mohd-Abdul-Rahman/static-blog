0c0e2114ed09856bea5cb75d5453fa0511cd2c06
Mohd-Abdul-Rahman <143195730+Mohd-Abdul-Rahman@users.noreply.github.com>
2024-01-20 15:37:28 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,7 +97,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body className="sm:bg-mobile xs:bg-hero-mobile md:bg-hero-mobile bg-no-repeat bg-cover bg-center bg-fixed lg:bg-hero bg-no-repeat bg-cover bg-center bg-fixed xl:bg-hero bg-no-repeat bg-cover bg-center bg-fixed">
+      <body className="sm:bg-hero-mobile xs:bg-hero-mobile md:bg-hero-mobile bg-no-repeat bg-cover bg-center bg-fixed lg:bg-hero bg-no-repeat bg-cover bg-center bg-fixed xl:bg-hero bg-no-repeat bg-cover bg-center bg-fixed">
 
         <div className="mx-auto max-w-xl px-6 ">
           {header}
